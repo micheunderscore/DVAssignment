992ef35b7180a8d4b3159a83ea43bb858e3d0669
Miche_ <51939599+micheunderscore@users.noreply.github.com>
2022-07-25 20:52:18 +0800

    admin: updated charts

--- src/js/akhi.js
+++ src/js/akhi.js
@@ -1,237 +1,173 @@
 // LINE CHART ==============================================================================
+var group = "All";
+d3.csv("./src/data/drivers.csv", (driverLookup) => {
+  d3.csv("./src/data/results.csv", function (dataset) {
+    function datasetLineChartChosen(usedGroup) {
+      let outData = {};
 
-d3.csv("./src/revenueData.csv", function (datasetLineChart) {
-  function datasetLineChartChosen(group) {
-    var ds = [];
-    for (x in datasetLineChart) {
-      if (datasetLineChart[x].group == group) {
-        ds.push(datasetLineChart[x]);
+      switch (usedGroup) {
+        case "All":
+          outData = d3
+            .nest()
+            .key((d) => d.driverId)
+            .rollup((leaves) => d3.mean(leaves, (d) => d.positionOrder))
+            .entries(dataset);
+          break;
+        default:
+          let temp = d3
+            .nest()
+            .key((d) => d.driverId)
+            .entries(dataset)
+            .filter((d) => {
+              return (
+                d.key == usedGroup &&
+                d.values.some(
+                  (d) =>
+                    d.positionOrder != undefined &&
+                    !Number.isNaN(d.positionOrder)
+                )
+              );
+            })[0].values;
+          outData = d3
+            .nest()
+            .key((d) => d.resultId)
+            .rollup((leaves) => d3.sum(leaves, (d) => d.positionOrder))
+            .entries(temp);
+          break;
       }
+
+      return outData;
     }
-    return ds;
-  }
 
-  function dsLineChartBasics() {
-    var margin = { top: 20, right: 10, bottom: 0, left: 50 },
-      width = 500 - margin.left - margin.right,
-      height = 150 - margin.top - margin.bottom;
-    return {
-      margin: margin,
-      width: width,
-      height: height,
+    function dsLineChart(usedGroup, usedColor) {
+      let groupedData = datasetLineChartChosen(usedGroup);
+
+      var margin = { top: 50, right: 0, bottom: 50, left: 0 },
+        width = 800 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
+
+      var x = d3
+        .scaleLinear()
+        .domain([0, groupedData.length - 1])
+        .range([0, width]);
+
+      var y = d3.scaleLinear().domain([0, 50]).range([0, height]);
+
+      var line = d3
+        .line()
+        .x((d, i) => x(i))
+        .y((d) => {
+          return y(d.value);
+        });
+
+      var svg = d3
+        .select("#lineChart")
+        .append("svg")
+        .attr("id", "thisLineChart")
+        .datum(groupedData)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+      var plot = svg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("id", "lineChartPlot");
+
+      plot
+        .append("path")
+        .attr("class", "line")
+        .attr("d", line)
+        .style("stroke", usedColor);
+
+      plot
+        .selectAll(".dot")
+        .data(groupedData)
+        .enter()
+        .append("circle")
+        .attr("class", "dot")
+        .attr("fill", (d) =>
+          d.value == d3.max(groupedData, (d) => d.value)
+            ? "red"
+            : d.value == d3.min(groupedData, (d) => d.value)
+            ? "green"
+            : "white"
+        )
+        .attr("opacity", (d) =>
+          d.value == d3.min(groupedData, (d) => d.value) ||
+          d.value == d3.max(groupedData, (d) => d.value)
+            ? 1
+            : 0
+        )
+        .attr("cx", line.x())
+        .attr("cy", line.y())
+        .attr("r", 3.5)
+        .attr("stroke", "lightgrey");
+
+      svg
+        .append("text")
+        .text(
+          `${
+            usedGroup == "All"
+              ? "Overall Driver"
+              : `${driverLookup[usedGroup - 1].forename} ${
+                  driverLookup[usedGroup - 1].surname
+                }'s`
+          } Career Average Grid Position`
+        )
+        .attr("id", "lineChartTitle1")
+        .attr("class", "titleText")
+        .attr("x", (margin.left + width + margin.right) / 2)
+        .attr("y", margin.top / 2);
+
+      plot
+        .append("text")
+        .text(d3.mean(groupedData, (d) => d.value).toFixed(0))
+        .attr("id", "lineChartTitle2")
+        .attr("x", width / 2)
+        .attr("y", height / 2);
+
+      svg
+        .append("circle")
+        .attr("cx", margin.left + 10)
+        .attr("cy", margin.top + height)
+        .attr("r", 6)
+        .style("fill", "green");
+      svg
+        .append("circle")
+        .attr("cx", margin.left + 180)
+        .attr("cy", margin.top + height)
+        .attr("r", 6)
+        .style("fill", "red");
+      svg
+        .append("text")
+        .attr("x", margin.left + 20)
+        .attr("y", margin.top + height)
+        .text(
+          `Best Grid Position #${d3
+            .min(groupedData, (d) => d.value)
+            .toFixed(0)}`
+        )
+        .style("font-size", "15px")
+        .attr("alignment-baseline", "middle");
+      svg
+        .append("text")
+        .attr("x", margin.left + 190)
+        .attr("y", margin.top + height)
+        .text(
+          `Worst Grid Position #${d3
+            .max(groupedData, (d) => d.value)
+            .toFixed(0)}`
+        )
+        .style("font-size", "15px")
+        .attr("alignment-baseline", "middle");
+    }
+
+    /* ** UPDATE CHART ** */
+    window.updateLineChart = function (usedGroup, colorChosen) {
+      d3.selectAll("#thisLineChart").remove();
+      dsLineChart(usedGroup, colorChosen);
     };
-  }
 
-  function dsLineChart() {
-    var firstDatasetLineChart = datasetLineChartChosen(group);
-
-    var basics = dsLineChartBasics();
-
-    var margin = basics.margin,
-      width = basics.width,
-      height = basics.height;
-
-    var xScale = d3.scale
-      .linear()
-      .domain([0, firstDatasetLineChart.length - 1])
-      .range([0, width]);
-
-    var yScale = d3.scale
-      .linear()
-      .domain([
-        0,
-        d3.max(firstDatasetLineChart, function (d) {
-          return d.performance;
-        }),
-      ])
-      .range([height, 0]);
-
-    var line = d3.svg
-      .line()
-      .x(function (d, i) {
-        return xScale(i);
-      })
-      .y(function (d) {
-        return yScale(d.performance);
-      });
-
-    var svg = d3
-      .select("#lineChart")
-      .append("svg")
-      .datum(firstDatasetLineChart)
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom);
-    // create group and move it so that margins are respected (space for axis and title)
-
-    svg
-      .append("circle")
-      .attr("cx", 10)
-      .attr("cy", 130)
-      .attr("r", 6)
-      .style("fill", "green");
-    svg
-      .append("circle")
-      .attr("cx", 160)
-      .attr("cy", 130)
-      .attr("r", 6)
-      .style("fill", "red");
-    svg
-      .append("text")
-      .attr("x", 20)
-      .attr("y", 130)
-      .text("Max Performance")
-      .style("font-size", "15px")
-      .attr("alignment-baseline", "middle");
-    svg
-      .append("text")
-      .attr("x", 170)
-      .attr("y", 130)
-      .text("Min Performance")
-      .style("font-size", "15px")
-      .attr("alignment-baseline", "middle");
-    var plot = svg
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .attr("id", "lineChartPlot");
-    /* descriptive titles as part of plot -- start */
-    var dsLength = firstDatasetLineChart.length;
-
-    plot
-      .append("text")
-      .text(firstDatasetLineChart[dsLength - 1].performance)
-      .attr("id", "lineChartTitle2")
-      .attr("x", width / 2)
-      .attr("y", height / 2);
-    /* descriptive titles -- end */
-
-    plot
-      .append("path")
-      .attr("class", "line")
-      .attr("d", line)
-      // add color
-      .attr("stroke", "lightgrey");
-
-    plot
-      .selectAll(".dot")
-      .data(firstDatasetLineChart)
-      .enter()
-      .append("circle")
-      .attr("class", "dot")
-      .attr("fill", function (d) {
-        return d.performance ==
-          d3.min(firstDatasetLineChart, function (d) {
-            return d.performance;
-          })
-          ? "red"
-          : d.performance ==
-            d3.max(firstDatasetLineChart, function (d) {
-              return d.performance;
-            })
-          ? "green"
-          : "white";
-      })
-      .attr("cx", line.x())
-      .attr("cy", line.y())
-      .attr("r", 3.5)
-      .attr("stroke", "lightgrey")
-      .append("title")
-      .text(function (d) {
-        return d.category + ": " + formatAsInteger(d.performance);
-      });
-
-    svg
-      .append("text")
-      .text("Overall Performance 2022")
-      .attr("id", "lineChartTitle1")
-      .attr("class", "titleText")
-      .attr("x", margin.left + (width + margin.right) / 2)
-      .attr("y", 10);
-  }
-
-  dsLineChart();
-
-  /* ** UPDATE CHART ** */
-
-  /* updates bar chart on request */
-  window.updateLineChart = function (group, colorChosen) {
-    var currentDatasetLineChart = datasetLineChartChosen(group);
-
-    var { margin, width, height } = dsLineChartBasics();
-
-    var xScale = d3.scale
-      .linear()
-      .domain([0, currentDatasetLineChart.length - 1])
-      .range([0, width]);
-
-    var yScale = d3.scale
-      .linear()
-      .domain([
-        0,
-        d3.max(currentDatasetLineChart, function (d) {
-          return d.performance;
-        }),
-      ])
-      .range([height, 0]);
-
-    var line = d3.svg
-      .line()
-      .x(function (d, i) {
-        return xScale(i);
-      })
-      .y(function (d) {
-        return yScale(d.performance);
-      });
-
-    var svg = d3.select("#lineChart svg");
-
-    var plot = d3.select("#lineChart").datum(currentDatasetLineChart);
-    var dsLength = currentDatasetLineChart.length;
-
-    plot
-      .select("#lineChartTitle2")
-      .text(currentDatasetLineChart[dsLength - 1].performance);
-
-    plot
-      .select("path")
-      .transition()
-      .duration(750)
-      .attr("class", "line")
-      .attr("d", line)
-      .style("stroke", colorChosen);
-
-    var path = plot
-      .selectAll(".dot")
-      .data(currentDatasetLineChart)
-      .transition()
-      .duration(750)
-      .attr("class", "dot")
-      .attr("fill", function (d) {
-        return d.performance ==
-          d3.min(currentDatasetLineChart, function (d) {
-            return d.performance;
-          })
-          ? "red"
-          : d.performance ==
-            d3.max(currentDatasetLineChart, function (d) {
-              return d.performance;
-            })
-          ? "green"
-          : "white";
-      })
-      .attr("cx", line.x())
-      .attr("cy", line.y())
-      .attr("r", 3.5)
-      .style("stroke", colorChosen);
-
-    path.selectAll("title").text(function (d) {
-      return d.category + ": " + formatAsInteger(d.performance);
-    });
-
-    svg
-      .selectAll("text.titleText")
-      .attr("x", (width + margin.left + margin.right) / 2)
-      .attr("y", 15)
-      .attr("text-anchor", "middle")
-      .text(group + "'s Performance 2022");
-  };
+    updateLineChart(group, "lightred");
+  });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,147 +1,173 @@
-var formatAsPercentage = d3.format("%"),
-  formatAsPercentage1Dec = d3.format(".1%"),
-  formatAsInteger = d3.format(","),
-  fsec = d3.time.format("%S s"),
-  fmin = d3.time.format("%M m"),
-  fhou = d3.time.format("%H h"),
-  fwee = d3.time.format("%a"),
-  fdat = d3.time.format("%d d"),
-  fmon = d3.time.format("%b");
+const colorPalette = [
+  "#9819f2",
+  "#baf279",
+  "#79f2d4",
+  "#1FCCF7",
+  "#79f2a8",
+  "#8bf279",
+  "#798af2",
+  "#DC0A3B",
+  "#79f279",
+  "#f29d79",
+  "#79f2d4",
+  "#dc79f2",
+  "#f23981",
+  "#79bff2",
+  "#ae79f2",
+  "#79dcf2",
+  "#f2bf79",
+  "#23326A",
+  "#bff279",
+  "#228988",
+  "#f2ca79",
+  "#D3ECCD",
+  "#f27991",
+  "#F14AFF",
+  "#941728",
+  "#33714C",
+  "#4DFD80",
+  "#EFBD26",
+  "#f279c2",
+  "#ff2800",
+  "#f29d79",
+  "#d679f2",
+  "#dc79f2",
+  "#f23981",
+  "#79bff2",
+];
 
-function dsPieChart() {
-  var dataset = [
-    { category: "Samad", measure: 0.3 },
-    { category: "Phang", measure: 0.25 },
-    { category: "Johan", measure: 0.15 },
-    { category: "Rita", measure: 0.05 },
-    { category: "Lenny", measure: 0.18 },
-    { category: "Pian", measure: 0.04 },
-    { category: "Siti", measure: 0.03 },
-  ];
-
-  var width = 400,
-    height = 400,
-    outerRadius = Math.min(width, height) / 2,
-    innerRadius = outerRadius * 0.999,
-    // for animation
-
-    innerRadiusFinal = outerRadius * 0.5,
-    innerRadiusFinal3 = outerRadius * 0.45,
-    color = d3.scale.category20();
-
-  var vis = d3
-    .select("#pieChart")
-    .append("svg:svg") //create the SVG element
-    .data([dataset]) //associate data
-    .attr("width", width) //set the width and height
-    .attr("height", height)
-    .append("svg:g") //make a group to the chart
-    //move the center of the pie chart
-    .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
-
-  var arc = d3.svg
-    .arc() // create <path> elements
-    .outerRadius(outerRadius)
-    .innerRadius(innerRadius);
-  // for animation
-
-  var arcFinal = d3.svg
-    .arc()
-    .innerRadius(innerRadiusFinal)
-    .outerRadius(outerRadius);
-
-  var arcFinal3 = d3.svg
-    .arc()
-    .innerRadius(innerRadiusFinal3)
-    .outerRadius(outerRadius);
-
-  var pie = d3.layout.pie().value(function (d) {
-    return d.measure;
-  });
-
-  var arcs = vis
-    .selectAll("g.slice")
-    .data(pie) //associate the generated pie data
-    .enter() //create <g> elements
-    .append("svg:g") //create a group to hold each slice
-    .attr("class", "slice") //set style in the slices
-    .on("mouseover", mouseover)
-    .on("mouseout", mouseout)
-    .on("click", up);
-
-  arcs
-    .append("svg:path")
-    //set the color for each slice
-    .attr("fill", function (d, i) {
-      return color(i);
-    })
-    .attr("d", arc) // actual SVG path
-    .append("svg:title") //mouseover title
-    .text(function (d) {
-      return d.data.category + ": " + formatAsPercentage(d.data.measure);
-    });
-
-  d3.selectAll("g.slice")
-    .selectAll("path")
-    .transition()
-    .duration(750)
-    .delay(10)
-    .attr("d", arcFinal);
-
-  // Add a label to the larger arcs, translated to the arc centroid
-  arcs
-    .filter(function (d) {
-      return d.endAngle - d.startAngle > 0.1;
-    })
-    .append("svg:text")
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .attr("transform", function (d) {
-      return "translate(" + arcFinal.centroid(d) + ")rotate(" + angle(d) + ")";
-    })
-    .text(function (d) {
-      return formatAsPercentage(d.value);
-    })
-
-    .text(function (d) {
-      return d.data.category;
-    });
-
-  // Computes the label angle of an arc, convert from rad to deg.
-  function angle(d) {
-    var a = ((d.startAngle + d.endAngle) * 90) / Math.PI - 90;
-    return a > 90 ? a - 180 : a;
-  }
-
-  // Pie chart title
-  vis
-    .append("svg:text")
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .text("Revenue 2022")
-    .attr("class", "title");
-
-  function mouseover() {
-    d3.select(this)
-      .select("path")
-      .transition()
-      .duration(750)
-      .attr("d", arcFinal3);
-  }
-
-  function mouseout() {
-    d3.select(this)
-      .select("path")
-      .transition()
-      .duration(750)
-      .attr("d", arcFinal);
-  }
-
-  function up(d, i) {
-    updateBarChart(d.data.category, color(i));
-    updateLineChart(d.data.category, color(i));
-  }
+for (var i = 0; i < 10; i++) {
+  colorPalette.push(...colorPalette);
 }
 
-dsPieChart(); //execute
 var group = "All";
+
+d3.csv("./src/data/drivers.csv", (driverLookup) => {
+  d3.csv("./src/data/results.csv", function (dataset) {
+    let groupedData = d3
+      .nest()
+      .key((d) => d.driverId)
+      .rollup((leaves) => d3.sum(leaves, (d) => d.points))
+      // .sort((d) => d.points)
+      .entries(dataset);
+    groupedData = groupedData.filter((d) => d.value > 0); // Only get drivers who have points
+    groupedData = groupedData
+      .sort((a, b) => d3.descending(a.value, b.value))
+      .slice(0, 30); // Get top 30 drivers
+
+    function dsPieChart(data) {
+      var width = 450,
+        height = 450,
+        margin = 0;
+
+      var radius = Math.min(width, height) / 2 - margin;
+
+      var Tooltip = d3
+        .select("#pieChart")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2.5px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        .style("position", "absolute");
+
+      function mousemove(d) {
+        var driver = driverLookup[d.data.value.key - 1];
+        Tooltip.html(
+          `${d.value}pts - ${driver.forename} ${driver.surname} (${driver.code})`
+        )
+          .style("border-color", colorPalette[d.data.value.key - 1])
+          .style("left", d3.event.pageX + 10 + "px")
+          .style("top", d3.event.pageY + 10 + "px");
+      }
+
+      function mouseleave(d) {
+        Tooltip.style("opacity", 0);
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .style("opacity", 0.5)
+          .style("stroke-width", "0px");
+      }
+
+      function mouseover(d) {
+        Tooltip.style("opacity", 1);
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .style("opacity", 1)
+          .style("stroke", "white")
+          .style("stroke-width", "15px");
+      }
+
+      function up(d) {
+        // updateBarChart(d.data.category, color(i));
+        updateLineChart(d.data.value.key, colorPalette[d.data.value.key - 1]);
+        group = d.data.value.key;
+
+        d3.selectAll("allSlices").style("opacity", (data) =>
+          group == data.data.value.key ? 1 : 0.5
+        );
+      }
+
+      var svg = d3
+        .select("#pieChart")
+        .append("svg")
+        .data(data)
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      var pie = d3.pie().value((d) => {
+        return d.value.value;
+      })(d3.entries(data));
+
+      var arc = d3
+        .arc()
+        .innerRadius(radius * 0.55)
+        .outerRadius(radius * 1);
+
+      svg
+        .selectAll("allSlices")
+        .data(pie)
+        .enter()
+        .append("path")
+        .attr("d", arc)
+        .attr("fill", (d) => colorPalette[d.data.value.key - 1])
+        .style("opacity", 0.6)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+        .on("click", up);
+
+      // Pie chart title
+      svg
+        .append("text")
+        .attr("text-anchor", "middle")
+        .attr("y", -20)
+        .text("Top 20")
+        .attr("class", "title");
+
+      // Pie chart title
+      svg
+        .append("text")
+        .attr("text-anchor", "middle")
+        .text("Formula One Drivers")
+        .attr("class", "title");
+
+      // Pie chart title
+      svg
+        .append("text")
+        .attr("text-anchor", "middle")
+        .attr("y", 20)
+        .text("(by total career points)")
+        .attr("class", "subtitle");
+    }
+
+    dsPieChart(groupedData);
+  });
+});
